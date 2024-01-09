06565e0b3f93352d01903c17d2b3c601c06e42ce
Le Van Tien <lvt030501@gmail.com>
2024-01-09 23:03:33 +0700

    Day-3 Modal Toggle

---
+++ Day-3/app.js
@@ -0,0 +1,17 @@
+var modal = document.querySelector(".modal");
+var btnOpen = document.querySelector(".open-modal-btn");
+var iconClose = document.querySelector(".modal__header i");
+var btnClose = document.querySelector(".modal__footer button");
+
+function toggleModal() {
+  modal.classList.toggle("hide");
+}
+
+btnOpen.addEventListener("click", toggleModal);
+btnClose.addEventListener("click", toggleModal);
+iconClose.addEventListener("click", toggleModal);
+modal.addEventListener("click", function (e) {
+  if (e.target == e.currentTarget) {
+    toggleModal();
+  }
+});
