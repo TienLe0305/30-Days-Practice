3ec6b0c6aa1202bb470ab33e29c038e40b493b05
Le Van Tien <lvt030501@gmail.com>
2024-01-20 01:19:44 +0700

    Day-13 Preview Image

---
+++ Day-13/app.js
@@ -0,0 +1,25 @@
+var upload = document.querySelector("#mypicture");
+var preview = document.querySelector(".preview");
+var error = document.querySelector(".error");
+
+upload.addEventListener("change", function (e) {
+  var file = upload.files[0];
+
+  if (!file.name.endsWith("jpg")) {
+    error.innerHTML = `Hinh anh hai duoc dinh dang jpg`;
+    return;
+  } else {
+    error.innerHTML = ``;
+  }
+
+  if (!file.size / (1024 * 1024) > 5) {
+    error.innerHTML = `Chi duoc upload anh < 5MB`;
+    return;
+  } else {
+    error.innerHTML = ``;
+  }
+
+  var img = document.createElement("img");
+  img.src = URL.createObjectURL(upload.files[0]);
+  preview.appendChild(img);
+});
